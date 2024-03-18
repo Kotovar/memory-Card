f29260a3696e124888045d2a60d0a33b458d5792
Kotovar <neasit3@gmail.com>
2024-03-18 14:31:29 +0700

    finish creating the game, add parallax library

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # memory-Card
 
-In developing
+This is a game where you have to click on images that were not in previous rounds
+
+Анимация
+
+[Live preview]()
+
+## How to use
+
+To use memory-Card, you need to have Node.js and npm installed on your system. You can clone this repository and run the following commands:
+
+`npm install` to install all dependencies
+
+`npm install -g vite @vitejs/plugin-react vite-plugin-simple-html` to install vite
+
+`npm run dev` to start the development server
+
+open `http://localhost:[path]` in your browser
+
+## How it works
+
+To create this application I used the following technologies:
+
+- React + JSX + JavaScript
+- SASS
+- Vite
+- react-parallax-tilt (Library)
+
+---
+
+The game has 3 difficulty levels, the maximum result is saved in the browser. The game takes a long time to load due to receiving images via the API, this was done to improve skills in working with the API.
+
+Изображение
+
+## Credits
+
+This project was inspired by [The Odin Project](https://www.theodinproject.com/lessons/node-path-react-new-memory-card), which is an open source curriculum for learning web development.
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,31 +1,34 @@
+import Tilt from 'react-parallax-tilt';
 import PropTypes from 'prop-types';
 import backImage from '../assets/images/static/backside.webp';
 
 export default function GameCard({name, id, isFlipping, img}) {
 	return (
-		<div className={`game-card`}>
-			<div
-				className={`game-card-front ${
-					isFlipping ? 'flip-in-front' : 'flip-out-front'
-				}`}
-			>
-				<img
-					src={img.src}
-					alt={`Изображение ${name}`}
-					className="gameImage"
-					data-id={id}
-				/>
-				<p>{name}</p>
-			</div>
+		<Tilt tiltMaxAngleX={20} tiltMaxAngleY={20}>
+			<div className={`game-card`}>
+				<div
+					className={`game-card-front ${
+						isFlipping ? 'flip-in-front' : 'flip-out-front'
+					}`}
+				>
+					<img
+						src={img.src}
+						alt={`Изображение ${name}`}
+						className="gameImage"
+						data-id={id}
+					/>
+					<p>{name}</p>
+				</div>
 
-			<div
-				className={`game-card-back ${
-					isFlipping ? 'flip-in-back' : 'flip-out-back'
-				} `}
-			>
-				<img src={backImage} alt="backside" />
+				<div
+					className={`game-card-back ${
+						isFlipping ? 'flip-in-back' : 'flip-out-back'
+					} `}
+				>
+					<img src={backImage} alt="backside" />
+				</div>
 			</div>
-		</div>
+		</Tilt>
 	);
 }
 
